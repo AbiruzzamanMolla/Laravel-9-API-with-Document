0439d2140b96a806ad168a8cc01e526bd34c1b12
Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2023-03-09 22:25:02 +0600

    Completed Authentication API Docomentaition

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,6 +16,44 @@ class LoginController extends Controller
         $this->authRepository = $authRepository;
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     tags={"Authentication"},
+     *     summary="Login using authentication credentials",
+     *     description="Login using authentication credentials of email and password",
+     *     operationId="login",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="email",
+     *                     description="User email address",
+     *                     type="string",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     description="user password",
+     *                     type="string"
+     *                 ),
+     *                required={"email", "password"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid input parameters"
+     *     )
+     * )
+     */
+
     public function login(LoginRequest $request)
     {
         try {
